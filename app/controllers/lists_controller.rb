ba492c5d0394d85af37e4a9e590e8b5079e15f0a
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-17 13:46:46 +0900

    [finish]解説1まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,6 +31,12 @@ class ListsController < ApplicationController
 			end
 	end
 
+	def destroy
+		@list = List.find(params[:id])
+		@list.destroy
+		redirect_to lists_path
+	end
+
 
 	private
 	def list_params
