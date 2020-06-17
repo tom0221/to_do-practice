fcac065c2a3602ed9992e72706d1745878c49d3f
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-17 12:09:56 +0900

    編集リンクまで

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,26 @@
+class ListsController < ApplicationController
+
+	def new
+		@list = List.new
+	end
+
+	def create
+		@list = List.new(list_params)
+		@list.save
+		redirect_to list_path(@list)
+	end
+
+	def index
+		@lists = List.all
+	end
+
+	def show
+		@list = List.find(params[:id])
+	end
+
+
+	private
+	def list_params
+		params.require(:list).permit(:name, :description)
+	end
+end
