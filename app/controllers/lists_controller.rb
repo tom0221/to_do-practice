7125d92f99b7e228df943274c81011f99125dddb
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-17 13:41:02 +0900

    [finish]編集機能

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,6 +18,19 @@ class ListsController < ApplicationController
 		@list = List.find(params[:id])
 	end
 
+	def edit
+		@list = List.find(params[:id])
+	end
+
+	def update
+		@list = List.find(params[:id])
+			if @list.update(list_params)
+				redirect_to lists_path
+			else
+				redirect_to edit_list_path
+			end
+	end
+
 
 	private
 	def list_params
